6c457b57e09ff3d46468fbf020ee3200e482d582
Lindroy <waxilong2016>
2017-05-05 18:06:40 +0800

    先选择日再选择月的功能还没有实现。

---
+++ app/src/main/java/com/linyulong/timepickerdemo/MainActivity.java
@@ -0,0 +1,68 @@
+package com.linyulong.timepickerdemo;
+
+import android.app.Activity;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
+
+import com.linyulong.timepickerdemo.widget.CustomDatePicker;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Locale;
+
+public class MainActivity extends Activity implements View.OnClickListener{
+    private RelativeLayout selectDate, selectTime;
+    private TextView currentDate, currentTime;
+    private CustomDatePicker datePicker;
+    private String now;
+    private String time;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        selectTime = (RelativeLayout) findViewById(R.id.selectTime);
+        selectTime.setOnClickListener(this);
+        selectDate = (RelativeLayout) findViewById(R.id.selectDate);
+        selectDate.setOnClickListener(this);
+        currentDate = (TextView) findViewById(R.id.currentDate);
+        currentTime = (TextView) findViewById(R.id.currentTime);
+        initPicker();
+    }
+
+    private void initPicker() {
+        SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd HH:mm", Locale.CHINA);
+        now = sdf.format(new Date());
+        time = sdf.format(new Date()).split(" ")[0];
+        //设置当前显示的时间
+//        currentDate.setText(now.split(" ")[0]);
+
+        datePicker = new CustomDatePicker(this, new CustomDatePicker.ResultHandler() {
+            @Override
+            public void handle(String time) {
+                currentDate.setText(time.split(" ")[0]);
+            }
+        },"2007-01-01 00:00", now);
+        datePicker.showSpecificTime(false);
+        datePicker.setIsLoop(false);
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.selectDate:
+                // 日期格式为yyyy-MM-dd
+                datePicker.show(time);
+                break;
+
+            case R.id.selectTime:
+                // 日期格式为yyyy-MM-dd HH:mm
+//                customDatePicker2.show(currentTime.getText().toString());
+                break;
+        }
+    }
+}
